c0bf885b6f7b8846ce6d973be32a6f55c99460c1
Max Grønlund <maxgronlund@bigmax.local>
2020-04-08 13:06:53 +0200

    removed prod.secret.exs to from .gitignore

--- config/prod.exs
+++ config/prod.exs
@@ -32,3 +32,19 @@ config :aoff_web,
     password: System.get_env("BASIC_AUTH_PASSWORD"),
     realm: System.get_env("BASIC_AUTH_REALM")
   ]
+
+config :arc,
+  storage: Arc.Storage.S3,
+  virtual_host: true,
+  # if using Amazon S3
+  bucket: System.get_env("AOFF_AWS_S3_BUCKET")
+
+config :ex_aws,
+  access_key_id: System.get_env("AOFF_AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.get_env("AOFF_AWS_SECRET_ACCESS_KEY"),
+  region: System.get_env("AOFF_AWS_REGION"),
+  s3: [
+    scheme: "https://",
+    host: "s3." <> System.get_env("AOFF_AWS_REGION") <> ".amazonaws.com",
+    region: System.get_env("AOFF_AWS_REGION")
+  ]
