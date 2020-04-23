e0fbd1bc3bab4acb8f9a727ab605ce9f743c0f42
Max Grønlund <maxgronlund@bigmax.local>
2020-04-23 18:15:35 +0200

    testing with cloudflare ip

--- apps/aoff_web/lib/aoff_web/controllers/shop/checkout_controller.ex
+++ apps/aoff_web/lib/aoff_web/controllers/shop/checkout_controller.ex
@@ -13,6 +13,8 @@ defmodule AOFFWeb.Shop.CheckoutController do
       )
 
     order = Users.get_order!(id)
+    merchantnumber = "8020677"
+    # System.get_env("EPAY_MERCHANT_NR")
 
 
     changeset = Users.change_order(order)
@@ -22,44 +24,14 @@ defmodule AOFFWeb.Shop.CheckoutController do
       "edit.html",
       changeset: changeset,
       order: order,
-      message: message
+      message: message,
+      merchantnumber: merchantnumber
     )
   end
 
-  def update(conn, %{"id" => id}) do
-    order = Users.get_order!(id)
-
-    # TODO here we are muging the response from the payment gateway
-
-    conn
-    |> redirect(to: Routes.shop_payment_accepted_path(conn, :index, order))
-
-    # changeset = Users.change_order(order)
-  end
-
-  # def show(conn, params) do
-  #   order = Users.get_order!(params["id"])
-  #   changeset = Users.change_order(order)
-  #   render(conn, "show.html", changeset: changeset, order: order)
-  # end
-
-  # def update(conn, %{"id" => id, "order" => order_params}) do
+  # def update(conn, %{"id" => id}) do
   #   order = Users.get_order!(id)
-
-  #   Users.update_order(order, order_params)
-
   #   conn
-  #   |> put_flash(:info, gettext("Your order is processed"))
-  #   |> redirect(to: Routes.user_order_path(conn, :show, order.user, order))
-
-  #   # case Users.update_order(order, order_params) do
-  #   #   {:ok, order} ->
-  #   #     conn
-  #   #     |> put_flash(:info, gettext("Order updated successfully."))
-  #   #     |> redirect(to: Routes.user_order_path(conn, :show, order.user, order))
-
-  #   #   {:error, %Ecto.Changeset{} = changeset} ->
-  #   #     render(conn, "edit.html", user: order.user, order: order, changeset: changeset)
-  #   # end
+  #   |> redirect(to: Routes.shop_payment_accepted_path(conn, :index, order))
   # end
 end
