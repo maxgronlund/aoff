046afe8347e45de87507f5b23db84eb0725e25a0
Max Grønlund <maxgronlund@bigmax.local>
2020-05-04 08:17:15 +0200

    link to blog_post on front page

--- apps/aoff_web/lib/aoff_web/controllers/info/about_controller.ex
+++ apps/aoff_web/lib/aoff_web/controllers/info/about_controller.ex
@@ -4,6 +4,7 @@ defmodule AOFFWeb.Info.AboutController do
   alias AOFF.Blogs
 
   def show(conn, %{"info_id" => blog_id, "id" => id}) do
+    conn = assign(conn, :page, :about_aoff)
     post = Blogs.get_post!(blog_id, id)
     render(conn, "show.html", post: post, blog: post.blog)
   end
