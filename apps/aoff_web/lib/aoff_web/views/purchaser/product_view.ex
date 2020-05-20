84bf8723f0241e9c255d5dffea41a71e4b1c0988
Max Grønlund <maxgronlund@bigmax.local>
2020-05-20 09:26:47 +0200

    updated some styling on the landing page and the signup form

--- apps/aoff_web/lib/aoff_web/views/purchaser/product_view.ex
+++ apps/aoff_web/lib/aoff_web/views/purchaser/product_view.ex
@@ -4,4 +4,8 @@ defmodule AOFFWeb.Purchaser.ProductView do
   def for_sale(for_sale) do
     if for_sale, do: gettext("Show in shop √"), else: gettext("Hidden")
   end
+
+  def show_on_landing_page(show_on_landing_page) do
+    if show_on_landing_page, do: gettext("Show on landing page √"), else: ""
+  end
 end
