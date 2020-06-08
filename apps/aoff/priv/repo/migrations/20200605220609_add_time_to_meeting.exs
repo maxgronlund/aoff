d65a554b815d7a53f70f4920450d2a01fb3ead0d
Max Grønlund <maxgronlund@bigmax.local>
2020-06-08 20:37:52 +0200

    bringing back a migration

--- apps/aoff/priv/repo/migrations/20200605220609_add_time_to_meeting.exs
+++ apps/aoff/priv/repo/migrations/20200605220609_add_time_to_meeting.exs
@@ -2,8 +2,8 @@ defmodule AOFF.Repo.Migrations.AddTimeToMeeting do
   use Ecto.Migration
 
   def change do
-    alter table("meetings") do
-      add :time, :time
-    end
+    # alter table("meetings") do
+    #   add :time, :time
+    # end
   end
 end
