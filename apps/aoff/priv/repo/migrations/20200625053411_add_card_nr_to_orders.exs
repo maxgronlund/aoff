afc9dc1ab82d0046fdc016fc9e568b62db1fa4bf
Max Grønlund <maxgronlund@bigmax.local>
2020-06-25 12:52:09 +0200

    fixed broken migration

--- apps/aoff/priv/repo/migrations/20200625053411_add_card_nr_to_orders.exs
+++ apps/aoff/priv/repo/migrations/20200625053411_add_card_nr_to_orders.exs
@@ -4,7 +4,7 @@ defmodule AOFF.Repo.Migrations.AddCardNrToOrders do
   def change do
     alter table(:orders) do
       add :card_nr, :string, default: ""
-      add :order_id, :string, default: ""
+      #add :order_id, :string, default: ""
     end
   end
 end
